9d93240c02355eb4150631157e72c49b8da8bcf2
Heli Ribeiro <heliribeiro2@gmail.com>
2020-07-06 15:00:26 -0300

    correção navbar

--- src/App.js
+++ src/App.js
@@ -115,24 +115,7 @@ function App() {
         <Carrinho callbackParent={(name, preco) => handleClickMinus(name, preco)} namePreco={namePreco} pokemonAtributos={pokemonAtributos} />
        
       </div>
-      <nav aria-label="Page navigation example">
-          <ul class="pagination justify-content-center">
-            <li class="page-item">
-              <a class="page-link" href="#" aria-label="Previous">
-                <span aria-hidden="true">&laquo;</span>
-              </a>
-            </li>
-            <li class="page-item"><a class="page-link" href="#">1</a></li>
-            <li class="page-item"><a class="page-link" href="#">2</a></li>
-            <li class="page-item"><a class="page-link" href="#">3</a></li>
-            <li class="page-item">
-              <a class="page-link" href="#" aria-label="Next">
-                <span aria-hidden="true">&raquo;</span>
-              </a>
-            </li>
-          </ul>
-        </nav>
-
+    
     </div>
   );
 
