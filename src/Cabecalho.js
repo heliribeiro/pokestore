0f1adf05fe72fc22a723d9e32979219c50e9f787
Heli Ribeiro <heliribeiro2@gmail.com>
2020-07-07 14:27:05 -0300

    mudança href do cabeçalho

--- src/Cabecalho.js
+++ src/Cabecalho.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Cabecalho() {
     return (
         <nav className="navbar navbar-dark bg-dark col-12">
-        <a href= './App' className="navbar-brand">PokeStore</a>
+        <a href= './index' className="navbar-brand">PokeStore</a>
         <form className="form-inline">
           <input className="form-control mr-sm-2" type="search" placeholder="Ex: pikachu" aria-label="Search" />
           <button className="btn btn-outline-success my-2 my-sm-0" type="
